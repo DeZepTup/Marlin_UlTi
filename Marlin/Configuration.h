573b2743201b085ef199eb40fd6047ff3db987d8
unknown <service@ivilol.ru>
2020-04-06 13:54:17 +0300

    Upgrade to version Marlin 2.0.5.3. UlTi steel v1.1.3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Ivilol.ru, UlTi Steel v1.1.2)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Ivilol.ru, UlTi Steel v1.1.3)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
