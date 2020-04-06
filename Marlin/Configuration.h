bbda043d1b649c56680c6bdbd42e62885e3b27a8
unknown <service@ivilol.ru>
2020-04-06 13:21:16 +0300

    Upgrade to version 2.0.5.3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "UlTi steel v1.1.2"
+#define CUSTOM_MACHINE_NAME "UlTi steel v1.1.3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
