7e274554cf48d8ef2c78609b68bcb731b93f72de
DeZepTup <dezeptup@gmail.com>
2021-04-18 01:14:25 +0300

    Update Configuration_adv.h
    Light update for the future

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2576,22 +2576,22 @@
    */
   //#define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
-  #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
-  #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
-  #define Z2_HYBRID_THRESHOLD      3
-  #define Z3_HYBRID_THRESHOLD      3
-  #define Z4_HYBRID_THRESHOLD      3
-  #define E0_HYBRID_THRESHOLD     30
-  #define E1_HYBRID_THRESHOLD     30
-  #define E2_HYBRID_THRESHOLD     30
-  #define E3_HYBRID_THRESHOLD     30
-  #define E4_HYBRID_THRESHOLD     30
-  #define E5_HYBRID_THRESHOLD     30
-  #define E6_HYBRID_THRESHOLD     30
-  #define E7_HYBRID_THRESHOLD     30
+  #define X_HYBRID_THRESHOLD     500  // [mm/s]
+  #define X2_HYBRID_THRESHOLD    500
+  #define Y_HYBRID_THRESHOLD     500
+  #define Y2_HYBRID_THRESHOLD    500
+  #define Z_HYBRID_THRESHOLD       500
+  #define Z2_HYBRID_THRESHOLD      500
+  #define Z3_HYBRID_THRESHOLD      500
+  #define Z4_HYBRID_THRESHOLD      500
+  #define E0_HYBRID_THRESHOLD     500
+  #define E1_HYBRID_THRESHOLD     500
+  #define E2_HYBRID_THRESHOLD     500
+  #define E3_HYBRID_THRESHOLD     500
+  #define E4_HYBRID_THRESHOLD     500
+  #define E5_HYBRID_THRESHOLD     500
+  #define E6_HYBRID_THRESHOLD     500
+  #define E7_HYBRID_THRESHOLD     500
 
   /**
    * Use StallGuard to home / probe X, Y, Z.
